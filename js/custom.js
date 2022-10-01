d6403245ca6098661dadcaabe9d09cc7314edc72
Khaled <khaledrayan40@gmail.com>
2022-10-01 22:19:04 +0600

    update-previous

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+// $(window).scroll(function(){
+//     $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+// });
+
+function changeBg() {
+    var navbar = document.getElementById('navbar');
+    var scrollValue = window.scrollY;
+    if (scrollValue < 150) {
+        navbar.classList.remove('bgColor')
+    } else {
+        navbar.classList.add('bgColor');
+    }
+}
+
+window.addEventListener('scroll', changeBg)
+
